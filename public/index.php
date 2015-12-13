f8de9053b9e2e03cd076768af172f30db484cd1d
MrPaulPhan <paul@mrpaulphan.com>
2015-12-12 22:59:09 -0500

    first commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Mail</title>
+</head>
+<body>
+    <form action="send.php" method="send.php">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+
+        <label for="email">email</label>
+        <input type="email" name="email" id="email">
+
+        <input type="submit" value="Submit">
+
+    </form>
+</body>
+</html>
---
+++ public/send.php
@@ -0,0 +1,20 @@
+<?php 
+
+# Include the Autoloader (see "Libraries" for install instructions)
+require '../vendor/autoload.php';
+use Mailgun\Mailgun;
+
+// Mailgun Sandbox API Key and Domain
+$mgClient = new Mailgun('key-43deec8f2e5725a3c383bf0999cc60d9');
+$domain = "sandboxb124b1de59a14ed38f8829231a236902.mailgun.org";
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => 'PHPMAIL <no-reply@mrpaulphan.com>',
+    'to'      => 'Paul <paul@mrpaulphan.com>',
+    'subject' => 'Hello',
+    'text'    => 'Testing some Mailgun awesomness! $name' . $name 
+));
